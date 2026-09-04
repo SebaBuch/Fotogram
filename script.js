d776714f8a62606419a10e7626e0fe3ed2d4661a
Sebastian <sebastian.buchholz98@gmail.com>
2026-09-04 14:02:03 +0200

    img added in js

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
-let items[];
+let items = ["assets/images/img1.png",
+    "assets/images/img2.png",
+    "assets/images/img3.png",
+    "assets/images/img4.png",
+    "assets/images/img5.png",
+    "assets/images/img6.png",
+    "assets/images/img7.png",
+    "assets/images/img8.png",
+    "assets/images/img9.png",
+    "assets/images/img10.png",
+    "assets/images/img11.png",
+    "assets/images/img12.png"];
+
+
 const imagesRef = document.getElementById('images');
 
-function init(){
+function init() {
     render();
 }
 
-function render(){
-    for(let i = 0; i<items.length; i=i+1){
-         imagesRef.innerHTML += templateLi(i);
+function render() {
+    for (let i = 0; i < items.length; i = i + 1) {
+        imagesRef.innerHTML += templateImg(i);
     }
 }
 
-function templateLi(i){
-    return `<img class='myImages'>${items[i]}</img>`;
+function templateImg(i) {
+    return `<img class='myImages' src="${items[i]}">`;
 }
+
+init();
